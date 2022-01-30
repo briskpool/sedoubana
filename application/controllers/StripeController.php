baebb4d49410a975cff459ba5bee2606ab1f65ae
briskpool <muzammil.mykhan@gmail.com>
2022-01-30 21:49:40 +0500

    Update StripeController.php

--- application/controllers/StripeController.php
+++ application/controllers/StripeController.php
@@ -45,7 +45,7 @@ class StripeController extends CI_Controller
             if (!empty($_POST['stripeToken'])) {
 
                 $token = $this->input->post('stripeToken');
-                $email =$user->email;
+                $email ="mudassir@gmail.com";
                 // $this->input->post('stripeEmail');
 
                 \Stripe\Stripe::setApiKey('sk_test_rCPBGpdY0OHBodwKzgWeBpkN00g7Hzfjrg');
