f3cd6a6496a1617cd854d884c1390f7f4c856eaf
briskpool <muzammil.mykhan@gmail.com>
2022-01-30 18:34:47 +0500

    commit

--- application/admin/views/auth-forgot-password.php
+++ application/admin/views/auth-forgot-password.php
@@ -11,7 +11,7 @@ $site_logo = front_url().$settings->site_logo;
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="<?= $site_logo ?>" alt="logo" style="width:60%;" class="shadow-light ">
+              <img src="<?= $site_logo ?>" alt="logo" style="width:60%;">
             </div>
 
             <div class="card card-primary">
