8871267ba1459f85c4335393d84fd11b868ea337
briskpool <muzammil.mykhan@gmail.com>
2022-01-30 17:32:19 +0500

    term coditions

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,11 +1,16 @@
  <div class="container">
    <div class="row">
-     <div class="col-md-12 text-center">
+     <div class="col-md-6 col-sm-12 text-center">
        <p class="mb-0">
          Copyright &copy;<script>
            document.write(new Date().getFullYear());
          </script> All rights reserved Sedoubana
        </p>
+     </div>
+     <div class="col-md-6 col-sm-12 text-center">
+       <p class="mb-0">
+        <a href="<?=base_url().'terms-condition'?>">Terms Conditions</a>
+       </p> 
      </div>
    </div>
  </div>
