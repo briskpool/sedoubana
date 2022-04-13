cb9e7fb87aaeb3775075aaca8956934948011251
mudassir1996 <beast.best2014@gmail.com>
2022-04-14 04:22:12 +0500

    Update Search_results.php

--- application/controllers/Search_results.php
+++ application/controllers/Search_results.php
@@ -86,7 +86,7 @@ class Search_results extends CI_controller
                 $status = $this->passengerTrips->postTrip($postArray);
                 if ($status['status']) {
                     $data['data'] = $status['data'];
-                    dd($$data);
+                    dd($data['data']);
                     $this->sendEmail($postArray);
                     $this->load->view('confirm_detail', $data);
                 } else {
